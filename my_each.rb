917279d3cfe7a5eae6297bbe08e086683bb3cbe2
daliayan <daliayan5@gmail.com>
2021-01-19 22:02:31 -0500

    "#committed"

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,56 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  i = 0
+  while i < words.length
+    yield(words[i])
+    i = i + 1
+  end
+   words
 end
+
+
+
+=begin
+
+def my_each(words)
+  i = 0
+  while i < words.length
+    yield(words[i])
+    i = i + 1
+  end
+   words
+end
+
+def my_each(words)
+  i = 0
+  while i < words.length
+    empty_words = 0
+    yield(words[i])
+    i = i + 1
+  end
+  empty_words << yield
+   words
+end
+  
+ while words
+  end
+  yield words
+  
+  def my_each(words)
+ while my_each(words) do |word|
+  puts word
+end
+  yield word
+end
+
+def my_each(words)
+  i = 0
+  while i < words.length
+    words_array = []
+    yield(words[i])
+    i = i + 1
+  end
+  words_array << yield
+  return words
+end
+
+=end
